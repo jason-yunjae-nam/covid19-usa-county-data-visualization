d91f2172e00db627b849686e47cbf4e58bf283e1
jason-yunjae-nam <jasonnam0416@gmail.com>
2022-06-17 11:35:34 +0900

    first commit

---
+++ corona-app/app.py
@@ -0,0 +1,7 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+from dash.dependencies import Input, Output
+import pandas as pd
+
