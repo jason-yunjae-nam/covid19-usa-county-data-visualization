8b08a3db55252795f26e48faa03cb54e77939312
jason-yunjae-nam <jasonnam0416@gmail.com>
2022-06-17 17:55:22 +0900

    code generation for us county choropleth map

--- corona-app/app.py
+++ corona-app/app.py
@@ -1,18 +1,59 @@
+from datetime import date
 import dash
 from dash import dcc
 from dash import html
 import plotly.graph_objects as go
+import plotly.express as px
 from dash.dependencies import Input, Output
 import pandas as pd
+import pycountry
+import addfips
 
-#baseURL = "cases_pt.csv"
+af = addfips.AddFIPS()
 
-data = pd.read_csv('Weekly_Province_Totals_Polygons.csv')
-
-def loadData (fileName, columnName):
-    data = data.drop(['Lat', 'Long'], axis=1).melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName).astype({'date':'datetime64[ns]', columnName:'Int64'}, errors='ignore')
-    data['Province/State'].fillna('<all>', inplace=True)
-    data[columnName].fillna(0, inplace=True)
+def loadData(fileName):
+    data = pd.read_csv(fileName)
+    data = data.drop(columns=['UID','iso2','iso3','code3','Province_State','Combined_Key','Country_Region','Lat','Long_'])
+    data = data.groupby(['FIPS', 'Admin2']).agg('sum')
     return data
 
-print(loadData("data.csv", "Province"))
+# def find_iso(name):
+#     try:
+#         return pycountry.countries.lookup(name).alpha_3
+#     except:
+#         return None
+
+def find_fips(name):
+    try:
+        return str(int(name)).zfill(5)
+    except:
+        return None
+
+def melt(df, date_list):
+    return pd.melt(df, id_vars=['fips', 'county'], value_vars=date_list)
+
+df = loadData('time_series_covid19_confirmed_US.csv')
+
+df['fips_int'] = df.index.get_level_values('FIPS')
+df['county'] = df.index.get_level_values('Admin2')
+df['fips'] = df['fips_int'].apply(find_fips)
+# df['iso_alpha_3'] = df['country'].apply(find_iso)
+
+date_list = list(df.columns)
+
+df = melt(df, date_list)
+
+print(df)
+
+map = px.choropleth(df,
+                    fips='fips',
+                    animation_frame='variable',
+                    color='value',
+                    hover_name='county',
+                    color_continuous_scale='Reds',
+                    range_color=[0, 2000],
+                    scope='usa'
+                    )
+
+map.show()
+map.write_html("example_map.html")
