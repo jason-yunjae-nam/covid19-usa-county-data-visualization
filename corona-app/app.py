4a8844304d408ac5b5097d8db75b23c5d591dc02
jason-yunjae-nam <jasonnam0416@gmail.com>
2022-06-17 13:50:16 +0900

    data swap 2

--- corona-app/app.py
+++ corona-app/app.py
@@ -5,11 +5,12 @@ import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 import pandas as pd
 
-baseURL = "https://resources-covid19canada.hub.arcgis.com/datasets/covid19canada::provincial-daily-totals/explore"
+#baseURL = "cases_pt.csv"
+
+data = pd.read_csv('Weekly_Province_Totals_Polygons.csv')
 
 def loadData (fileName, columnName):
-    data = pd.read_csv(baseURL + fileName)
-    #.drop(['Lat', 'Long'], axis=1).melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName).astype({'date':'datetime64[ns]', columnName:'Int64'}, errors='ignore')
+    data = data.drop(['Lat', 'Long'], axis=1).melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName).astype({'date':'datetime64[ns]', columnName:'Int64'}, errors='ignore')
     data['Province/State'].fillna('<all>', inplace=True)
     data[columnName].fillna(0, inplace=True)
     return data
