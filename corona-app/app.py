86bc5be9307c135a9ffc0c0e7117a77678ebf524
jason-yunjae-nam <jasonnam0416@gmail.com>
2022-06-17 13:20:55 +0900

    added .csv file

--- corona-app/app.py
+++ corona-app/app.py
@@ -1,7 +1,17 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 import pandas as pd
 
+baseURL = "https://resources-covid19canada.hub.arcgis.com/datasets/covid19canada::provincial-daily-totals/explore"
+
+def loadData (fileName, columnName):
+    data = pd.read_csv(baseURL + fileName)
+    #.drop(['Lat', 'Long'], axis=1).melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName).astype({'date':'datetime64[ns]', columnName:'Int64'}, errors='ignore')
+    data['Province/State'].fillna('<all>', inplace=True)
+    data[columnName].fillna(0, inplace=True)
+    return data
+
+print(loadData("data.csv", "Province"))
